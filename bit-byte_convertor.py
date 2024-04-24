fd3c72101b54efd02f32b6b029d4952f4cbefa1b
Elijah Apreko <elijahapreko@gmail.com>
2024-04-24 18:58:06 +0000

    first commit

---
+++ bit-byte_convertor.py
@@ -0,0 +1,9 @@
+"""
+Elijah will handle the display of the introductory message
+Jemima will handle the user input
+Justice will handle the bytes to bit conversion
+Tracy will handle the bits to bytes conversion
+Martha will handle the exiting of the program
+Nana Ama will handle the invalid choice
+"""
+
